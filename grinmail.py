7730a36d7d39191e1b4fce199885943cbc9e3309
aixinrock <aixinrock@qq.com>
2021-05-11 22:02:01 +0800

    解决只能处理最新一封邮件的问题，变为逐个处理取得的所有新邮件

--- grinmail.py
+++ grinmail.py
@@ -88,25 +88,23 @@ def send_mail(sender,content,slate_file):
     logger.info('已回复GrinMail交易邮件，bingo！')
 
 def main():
-    try:
-        new_mail = server.get_latest()
-    except:
-        return
-
-    global mail_id
-    if new_mail['id'] == mail_id:
-        return
-
-    logger.info('收到一封新邮件 %s',new_mail['subject'])
-    slatepack = polling_mail(new_mail)
-    if slatepack:
-        pattern = re.compile(r'<(.*?)>')
-        sender = re.search(pattern,new_mail['from']).group(1)
-        logging.info('终端开始处理Slatepack1数据...')
-        result = terminal(slatepack)
-        content,slate_file = handle_result(result)
-        send_mail(sender,content,slate_file)
-    mail_id += 1
+    mail_id = email_provider(user)
+    while True:
+        time.sleep(60)
+        mails = server.get_mails(start_index=mail_id + 1)
+        if mails:
+            logger.info('共收到 %s 封新邮件',len(mails))
+            for new_mail in mails:
+                logger.info('收到一封新邮件 %s',new_mail['subject'])
+                slatepack = polling_mail(new_mail)
+                if slatepack:
+                    pattern = re.compile(r'<(.*?)>')
+                    sender = re.search(pattern,new_mail['from']).group(1)
+                    logging.info('终端开始处理Slatepack1数据...')
+                    result = terminal(slatepack)
+                    content,slate_file = handle_result(result)
+                    send_mail(sender,content,slate_file)
+            mail_id += len(mails)
 
 
 if __name__ == '__main__':
@@ -116,7 +114,6 @@ if __name__ == '__main__':
     password = config.get('login','email_password')
 
     server = zmail.server(user,password)
-    mail_id = email_provider(user)
     
     logging.basicConfig(level=logging.DEBUG,
     filename='log',
@@ -125,6 +122,4 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.info('GrinMail上线，开始运行...')
 
-    while True:
-        main()
-        time.sleep(60)
+    main()
