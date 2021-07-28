f9d8d1eb226630de1b5db045f2473ba103fb618b
aixinrock <aixinrock@qq.com>
2021-07-28 19:50:32 +0800

    代码重构，但未用wallet类，增加掉线重连功能

--- grinmail-win.py
+++ grinmail-win.py
@@ -8,110 +8,77 @@ import zmail
 
 
 def terminal(slatepack):
-    try:
-        child = wexpect.spawn('grin-wallet receive')
-        child.expect('Password:')
-        wallet_password = config.get('login','wallet_password')
-        child.sendline(wallet_password)
-        child.expect('message:')
-        child.sendline(slatepack)
-        child.expect(wexpect.EOF)
-        result = child.before
-    except:
-        result = 'Slatepack data is wrong, please verify and resend.'
-    logger.debug('终端返回结果：%s',result)
+    child = wexpect.spawn('grin-wallet receive -m')
+    child.expect('Password:')
+    wallet_password = config.get('login','wallet_password')
+    child.sendline(wallet_password)
+    child.expect('message:')
+    child.sendline(slatepack)
+    child.expect(wexpect.EOF)
+    result = child.before
 
-    return result
+    pattern = re.compile(r'C.*?S2.slatepack')
+    slate_file = re.search(pattern,result).group()
+    with open(slate_file,'r') as f:
+        slate2 = f.read()
+    return (slate2,slate_file)
 
-def handle_result(result):
-    if 'Slatepack data follows' in result:
-        pattern = re.compile(r'C.*?S2.slatepack')
-        slate_file = re.search(pattern,result).group()
-        logger.debug('Slatepack2文件存储目录：%s',slate_file)
-        with open(slate_file,'r') as f:
-            text = f.read()
-        logger.debug('生成回应Slatepack2数据：%s',text)
-        logger.info('已生成回应Slatepack2数据')
-        content = 'GrinMail交易邮件已收到，以下是生成回应的Slatepack2数据，'\
-                  '你可以将其复制后直接粘贴进钱包，或者将附件下载提交到钱包，'\
-                 f'以完结交易的方式提交上链。\n{text}'
-        return (content,slate_file)
 
-    elif 'Transaction recieved and sent back to sender' in result:
-        logger.info('已通过TOR方式自动完成交互任务。')
-        content = '你的Grin交易已通过Tor自动完成交互，请耐心等待区块确认以完成上链！'
-        return (content,None)
-
-    elif 'has already been received' in result:
-        logger.info('此交易重复')
-        content = '你之前提交的Grin交易已收到，请不要重复发送同一笔交易。'
-        return (content,None)
-    else:
-        logger.info('Slatepack 数据错误')
-        content = '对不起，你所发送的 Slatepack 数据有误，请检查验证或重新生成后再次发送。'
-        return (content,None)
-
-def polling_mail(new_mail): 
+def get_slatepack(): 
     if new_mail['attachments']:
-        if 'slatepack' in new_mail['attachments'][0][0]:
+        if '.slatepack' in new_mail['attachments'][0][0]:
             slatepack = new_mail['attachments'][0][1].decode().replace('\n','')
-            logger.debug('附件中收到的Slatepack1数据：%s',slatepack)
+            logger.debug('附件中匹配到slatepack数据：%s',slatepack)
             return slatepack
-        else:
-            logger.info('附件中不含slatepack数据，开始检测正文内容...')
 
     try:
         mail_content = new_mail['content_text'][0]
         pattern = re.compile(r'BEGINSLATEPACK.[\s\S]*?ENDSLATEPACK.')
         slatepack = re.search(pattern,mail_content).group()
         slatepack = ' '.join(slatepack.split())
-        logger.debug('正文中匹配到的Slatepack1数据：%s',slatepack)
+        logger.debug('正文中匹配到slatepack数据：%s',slatepack)
         return slatepack
     except:
-        logger.info('正文中不含slatepack数据，此邮件不是Grin交易邮件。')
+        logger.info('正文和附件中均不含slatepack数据，此邮件不是Grin交易邮件。')
         return
 
-def send_mail(sender,content,slate_file):
-    mail = {'subject':'GrinMail 交易邮件（Grin trading mail）',
+
+def send_mail(sender,subject,content,slate_file):
+    mail = {'subject':subject,
             'content_text':content,
             'attachments':slate_file,
     }
-    server.send_mail(sender,mail)
-    logger.info('已回复GrinMail交易邮件，bingo！')
-
-def gmail():
     try:
-        new_mail = server.get_latest()
+        server.send_mail(sender,mail)
     except:
-        return
-    logger.info('收到一封新邮件 %s',new_mail['subject'])
-    slatepack = polling_mail(new_mail)
+        logger.info('由于网络原因，邮件未发送成功，60秒后重新发送。')
+        time.sleep(60)
+        send_mail(sender,subject,content,slate_file)
+
+
+def receive_mail():
+    logger.info('收到新邮件 "%s"',new_mail['subject'])
+    slatepack = get_slatepack()
     if slatepack:
+        logging.info('终端开始处理slatepack数据...')
+        try:
+            slate2,slate_file = terminal(slatepack)
+        except:
+            logging.info('Grin交易重复，或slatepack数据错误，请验证后再发！')
+            return
+
+        content = '收到你的Grin交易请求，生成回应slatepack2信息如下：\n'\
+              'Received your Grin transaction request, slatepack2 message has been generated in response to you: \n'\
+              '————————————————————————'\
+              f'\n{slate2}\n'\
+              '————————————————————————\n'\
+              '请将以上信息复制粘贴到钱包，或将附件提交到钱包，来完结交易以提交上链。如果你使用GrinMail工具，则无需任何操作，自动回复。\n'\
+              'Please copy and paste the slatepack message into the wallet, or submit the attachment to the wallet, finalize the transaction and submit the blockchain. if you use the GrinMail, you will reply automatically without any action.'
+        subject = '[GrinMail] 请完结Grin交易（Finalize Grin slatepack2）'
         pattern = re.compile(r'<(.*?)>')
         sender = re.search(pattern,new_mail['from']).group(1)
-        logging.info('终端开始处理Slatepack1数据...')
-        result = terminal(slatepack)
-        content,slate_file = handle_result(result)
-        send_mail(sender,content,slate_file)
-
-def main():
-    mail_id = server.get_latest()['id']
-    while True:
-        time.sleep(150)
-        mails = server.get_mails(start_index=mail_id + 1)
-        if mails:
-            logger.info('共收到 %s 封新邮件',len(mails))
-            for new_mail in mails:
-                logger.info('收到一封新邮件 %s',new_mail['subject'])
-                slatepack = polling_mail(new_mail)
-                if slatepack:
-                    pattern = re.compile(r'<(.*?)>')
-                    sender = re.search(pattern,new_mail['from']).group(1)
-                    logging.info('终端开始处理Slatepack1数据...')
-                    result = terminal(slatepack)
-                    content,slate_file = handle_result(result)
-                    send_mail(sender,content,slate_file)
-            mail_id += len(mails)
+        send_mail(sender,subject,content,slate_file)
+        logger.info('回复Grin交易邮件成功，Bingo！')
 
 
 if __name__ == '__main__':
@@ -129,9 +96,27 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.info('GrinMail上线，开始运行...')
 
-    if user.endswith('@gmail.com'):
-        while True:
-            gmail()
-            time.sleep(150)
-    else:
-        main()
+    if not user.endswith('@gmail.com'):
+        mail_id = server.get_latest()['id']
+        logging.info('邮件ID获取成功，开始收取邮件')
+            
+    while True:
+        time.sleep(20)
+        if user.endswith('@gmail.com'):
+            try:
+                new_mail = server.get_latest()
+            except:
+                continue
+            receive_mail()
+        else:
+            try:
+                mails = server.get_mails(start_index=mail_id + 1)
+            except:
+                logging.info('网络原因断开，60秒后重试')
+                time.sleep(60)
+                continue
+            if mails:
+                logger.info('共收到 %s 封新邮件',len(mails))
+                for new_mail in mails:
+                    receive_mail()
+                mail_id += len(mails)
